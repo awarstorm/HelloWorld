25443fb2a3576f2ec42895f6fbf0928aa83d3e0e
Anthony Warstorm <awarstorm@vermillion.eaglecrk.local>
2017-12-07 16:07:03 -0600

    Closed Buffered Reader

--- src/com/eaglecrk/awarstorm/HelloWorld.java
+++ src/com/eaglecrk/awarstorm/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.eaglecrk.awarstorm;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 import com.eaglecrk.awarstorm.input.ConsoleInputHandler;
@@ -10,28 +11,35 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		boolean again = false;
-		System.out.println("Hello World!");
+		BufferedReader reader = null;
 		
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		ConsoleInputHandler H = new ConsoleInputHandler();
-		
-		do {
-		
-			int addend1 = H.getInteger(reader, "Enter First Integer");
-			int addend2 = H.getInteger(reader, "Enter Next Integer");
-			int sum = AddingModule.sum(addend1, addend2);
+		try {
+			System.out.println("Hello World!");
+	
+			reader = new BufferedReader(new InputStreamReader(System.in));
+			ConsoleInputHandler H = new ConsoleInputHandler();
 			
-			System.out.println("Sum: " + sum);	
-			System.out.println("");
-			again = H.getBoolean(reader, "Would you like to add together more numbers?");
+			do {
 			
-			System.out.println("");
-		} while(again);
-		
-		System.out.println("Goodbye!");
-			
+				int addend1 = H.getInteger(reader, "Enter First Integer");
+				int addend2 = H.getInteger(reader, "Enter Next Integer");
+				int sum = AddingModule.sum(addend1, addend2);
+				
+				System.out.println("Sum: " + sum);	
+				System.out.println("");
+				again = H.getBoolean(reader, "Would you like to add together more numbers?");
+				
+				System.out.println("");
+			} while(again);
+			System.out.println("Goodbye!");			
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 	}
-
 }
